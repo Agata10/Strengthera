90e358dba93b855d86ec417e166ad97bdb9c4dd0
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-18 16:56:25 -0600

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+import Nav from "./Nav"
+const Header = () => {
+  return (
+    <div>
+        <img src="" alt="logo" />
+        <h1>StrengthEra</h1>
+<Nav/>
+    </div>
+  )
+}
+
+export default Header
