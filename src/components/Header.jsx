83c771af785bccc5befc9c2f31815812ddb487b8
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-19 16:16:50 -0600

    Add style to header and nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,11 @@ import dumbell from '../assets/dumbell.png';
 import Nav from './Nav';
 const Header = () => {
   return (
-    <div className="flex justify-between px-20 h-20 border-red-200 ">
+    <div className="flex justify-between items-center px-20 h-20">
       <div className="flex gap-3 justify-center">
-        <img src={dumbell} alt="logo" />
-        <h1 className="text-4xl font-logo relative top-5">
-          {' '}
-          Strength<span className="text-pink-950">Era</span>
+        <img src={dumbell} alt="logo" className="w-12 h-12 md:w-16 md:h-16" />
+        <h1 className="text-2xl md:text-4xl font-logo relative right-3 top-3 md:top-5 text-orange-400">
+          Strength<span className="text-blue-400">Era</span>
         </h1>
       </div>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
-    <ul>
-      <li className="">
+    <ul className="flex justify-center items-center gap-2 text-slate-700">
+      <li className="hover:underline underline-offset-4 decoration-orange-600">
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li className="hover:underline underline-offset-4 decoration-orange-600">
         <Link to="workouts">Workouts</Link>
       </li>
-      <li>
+      <li className="hover:underline underline-offset-4 decoration-orange-600">
         <Link to="/exercises">Exercises</Link>
       </li>
     </ul>
