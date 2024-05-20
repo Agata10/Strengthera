48285386b8d0320b6b96d72bacd9e54f42628c55
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-20 11:29:48 -0600

    Add styling to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
 const Card = ({ exercise }) => {
   const { id, gifUrl, name, bodyPart, target } = exercise;
   return (
-    <div id={id}>
+    <div
+      id={id}
+      className="border-t-2 border-blue-400 pt-4 flex flex-col gap-3 font-logo cursor-pointer"
+    >
       <img src={gifUrl} alt={name} />
-      <h4>{name}</h4>
-      <p>
-        {bodyPart}
-        <span>{target}</span>
-      </p>
+      <h4 className="text-lg font-semibold">
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </h4>
+      <div className="flex gap-2">
+        <button className="bg-orange-600 px-3 py-1 rounded-full text-sm cursor-default">
+          {bodyPart}
+        </button>
+        <button className="bg-orange-400 px-2 rounded-full text-sm cursor-default">
+          {target}
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/CardsWrapper.jsx
+++ src/components/CardsWrapper.jsx
@@ -4,7 +4,11 @@ const CardsWrapper = ({ exercises }) => {
     return <Card key={e.id} exercise={e} />;
   });
 
-  return <div>{exercisesData}</div>;
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 mx-auto w-8/12 lg:w-10/12 gap-10 mt-10">
+      {exercisesData}
+    </div>
+  );
 };
 
 export default CardsWrapper;
