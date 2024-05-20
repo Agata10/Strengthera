b0402975d1bac4b24fbfe90207663605b2e643e0
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-20 10:18:37 -0600

    Add fetching data from search input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,53 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 const SearchBar = () => {
-  const inputRef = useRef('');
-  useEffect(() => {
-    const getExercises = async (name) => {
-      const options = {
-        method: 'GET',
-        url: `https://exercisedb.p.rapidapi.com/exercises/name/${name}`,
-        params: {
-          offset: '0',
-          limit: '10',
-        },
-        headers: {
-          'x-rapidapi-key': import.meta.env.VITE_API_KEY,
-          'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-        },
-      };
-      console.log(options.headers['x-rapidapi-key']);
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+  const [searchInput, setSearchInput] = useState('');
+  const [exercises, setExercises] = useState('');
+
+  const getExercises = async () => {
+    const options = {
+      method: 'GET',
+      url: `https://exercisedb.p.rapidapi.com/exercises`,
+      params: {
+        offset: '0',
+        limit: '500',
+      },
+      headers: {
+        'x-rapidapi-key': import.meta.env.VITE_API_KEY,
+        'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+      },
     };
-    getExercises(inputRef.current);
-  }, [inputRef]);
+    try {
+      if (searchInput) {
+        const response = await axios.request(options);
+        const filterData = response.data.filter(
+          (exercise) =>
+            exercise.name.includes(searchInput) ||
+            exercise.bodyPart.includes(searchInput) ||
+            exercise.target.includes(searchInput) ||
+            exercise.equipment.includes(searchInput)
+        );
+        console.log(filterData);
+        setExercises(filterData);
+        return filterData;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="w-full flex justify-center items-center bg-slate-100 h-20">
-      <form className="flex gap-6 w-full justify-center h-8">
+      <form
+        className="flex gap-6 w-full justify-center h-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          getExercises();
+        }}
+      >
         <input
-          ref={inputRef}
-          onChange={(e) => (inputRef.current = e.target.value)}
+          onChange={(e) => setSearchInput(e.target.value.toLowerCase())}
+          value={searchInput}
           type="text"
           placeholder="Search exercises..."
           className="w-4/12 md:w-3/12 shadow shadow-blue-400 rounded-md px-2 hover:shadow-md hover:shadow-blue-400 outline-blue-400"
