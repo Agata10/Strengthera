1eedab2637e2e09fe5561eb53471571eb9772f65
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-22 18:42:59 -0600

    Add icon to delete exercise

--- src/components/DaysHolder.jsx
+++ src/components/DaysHolder.jsx
@@ -48,69 +48,56 @@ const DaysHolder = () => {
 
   const workoutThatDay = workout.map((days) => {
     if (days.day === day) {
+      if (days.exercises.length === 0) {
+        return 'Rest day';
+      }
       const exercises = days.exercises.map((exercise) => {
-        if (!exercise) {
-          return null;
-        }
         return (
           <li key={exercise.name}>
-            <table>
-              <tr className="flex items-center justify-center">
-                <td className="mr-8 w-56">
-                  {exercise.name.charAt(0).toUpperCase() +
-                    exercise.name.slice(1)}
-                </td>
-                <td>
-                  <input
-                    className="w-6 text-left placeholder:text-slate-800"
-                    placeholder={exercise.sets}
-                    onBlur={(e) =>
-                      handleSets(day, exercise.name, Number(e.target.value))
-                    }
+            <div className="flex">
+              <p className="mr-8 w-56">
+                {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
+              </p>
+              <input
+                className="w-6 text-left placeholder:text-slate-800"
+                placeholder={exercise.sets}
+                onBlur={(e) =>
+                  handleSets(day, exercise.name, Number(e.target.value))
+                }
+              />
+              <span className="mr-8">sets</span>
+              <input
+                className="w-6 text-left placeholder:text-slate-800"
+                placeholder={exercise.reps}
+                onBlur={(e) =>
+                  handleReps(day, exercise.name, Number(e.target.value))
+                }
+              />
+              <span className="mr-8">reps</span>
+              <button
+                className="flex items-center justify-center"
+                onClick={() => handleDelete(day, exercise.name)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="rgb(37 99 235)"
+                  className="size-5 hover:scale-110"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                   />
-                  <span className="mr-8">sets</span>
-                </td>
-                <td>
-                  <input
-                    className="w-6 text-left placeholder:text-slate-800"
-                    placeholder={exercise.reps}
-                    onBlur={(e) =>
-                      handleReps(day, exercise.name, Number(e.target.value))
-                    }
-                  />
-                  <span className="mr-8">reps</span>
-                </td>
-                <td>
-                  <button
-                    className="flex items-center justify-center"
-                    onClick={() => handleDelete(day, exercise.name)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="rgb(37 99 235)"
-                      className="size-5 hover:scale-110"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </td>
-              </tr>
-            </table>
+                </svg>
+              </button>
+            </div>
           </li>
         );
       });
-      if (exercises.length === 0) {
-        return 'Rest day';
-      } else {
-        return exercises;
-      }
+      return exercises;
     }
   });
   return (
