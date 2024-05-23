27c750aedb82c074fd06d66a648e9286b2dd46a1
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-23 10:08:31 -0600

    Add basic styling for slide

--- src/components/BodyParts.jsx
+++ src/components/BodyParts.jsx
@@ -3,7 +3,10 @@ import axios from 'axios';
 import { bodyPartsListOptions } from '../utils/fetchData';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { breakpoints } from '../utils/breakpoints';
-import { Pagination } from 'swiper/modules';
+import { Virtual, Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const BodyParts = () => {
   const [bodyParts, setBodyParts] = useState(
     null || JSON.parse(localStorage.getItem('bodyParts'))
@@ -29,7 +32,7 @@ const BodyParts = () => {
       <SwiperSlide
         key={crypto.randomUUID()}
         id={part}
-        className="cursor-pointer"
+        className="cursor-pointer flex flex-col justify-center items-center"
       >
         <img src="" alt={part} />
         <h4>{part}</h4>
@@ -38,13 +41,12 @@ const BodyParts = () => {
   });
   return (
     <Swiper
-      className="mySwiper  w-8/12 md:w-10/12 h-16 px-10 md:px-20"
+      className="mySwiper  w-8/12 md:w-10/12 h-16 px-10 md:px-20 my-20"
       spaceBetween={10}
       breakpoints={breakpoints}
-      modules={[Pagination]}
-      pagination={{
-        clickable: true,
-      }}
+      modules={[Navigation]}
+      centeredSlides={true}
+      navigation={true}
       onClick={(swipper) => setBodyPart(swipper.clickedSlide.id)}
     >
       {console.log(bodyPart)}
