baba04931ef1567ba1cbfbf91353a5c68706a95c
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-23 09:55:18 -0600

    Add swiper to body parts and handle click

--- src/components/BodyParts.jsx
+++ src/components/BodyParts.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { bodyPartsListOptions } from '../utils/fetchData';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { breakpoints } from '../utils/breakpoints';
+import { Pagination } from 'swiper/modules';
 const BodyParts = () => {
   const [bodyParts, setBodyParts] = useState(
     null || JSON.parse(localStorage.getItem('bodyParts'))
   );
+  const [bodyPart, setBodyPart] = useState('back');
+
   useEffect(() => {
     if (!bodyParts) {
       const getBodyPartsList = async () => {
@@ -20,8 +24,33 @@ const BodyParts = () => {
       getBodyPartsList();
     }
   }, [bodyParts]);
-
-  return <div>BodyParts</div>;
+  const bodyPartSlide = bodyParts.map((part) => {
+    return (
+      <SwiperSlide
+        key={crypto.randomUUID()}
+        id={part}
+        className="cursor-pointer"
+      >
+        <img src="" alt={part} />
+        <h4>{part}</h4>
+      </SwiperSlide>
+    );
+  });
+  return (
+    <Swiper
+      className="mySwiper  w-8/12 md:w-10/12 h-16 px-10 md:px-20"
+      spaceBetween={10}
+      breakpoints={breakpoints}
+      modules={[Pagination]}
+      pagination={{
+        clickable: true,
+      }}
+      onClick={(swipper) => setBodyPart(swipper.clickedSlide.id)}
+    >
+      {console.log(bodyPart)}
+      {bodyPartSlide}
+    </Swiper>
+  );
 };
 
 export default BodyParts;
