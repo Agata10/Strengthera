675fa921d1d983293c712dac5b1fc9ac1f6a79df
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-07-24 12:16:40 -0600

    Add different body parts images

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,40 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Nav = ({ visible }) => {
   return (
     <ul
       className={` ${
         visible ? 'flex flex-col w-full items-start mt-3 pl-10' : 'hidden'
-      } md:flex md:flex-row md:justify-center md:items-center gap-2 md:mt-0 md:pl-0 text-slate-700`}
+      } md:flex md:flex-row md:justify-center md:items-center gap-2 md:mt-0 md:pl-0 text-slate-600`}
     >
       <li className="hover:underline underline-offset-4 decoration-orange-600">
-        <Link to="/">Home</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'underline underline-offset-4 decoration-orange-600' : ''
+          }
+        >
+          Home
+        </NavLink>
       </li>
       <li className="hover:underline underline-offset-4 decoration-orange-600">
-        <Link to="/workouts">Workouts</Link>
+        <NavLink
+          to="/workouts"
+          className={({ isActive }) =>
+            isActive ? 'underline underline-offset-4 decoration-orange-600' : ''
+          }
+        >
+          Workouts
+        </NavLink>
       </li>
       <li className="hover:underline underline-offset-4 decoration-orange-600">
-        <Link to="/exercises">Exercises</Link>
+        <NavLink
+          to="/exercises"
+          className={({ isActive }) =>
+            isActive ? 'underline underline-offset-4 decoration-orange-600' : ''
+          }
+        >
+          Exercises
+        </NavLink>
       </li>
     </ul>
   );
